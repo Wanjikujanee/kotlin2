c3bd6f3202ccabdf72633a46522d17a1c03d3428
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-02-22 21:50:32 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    var name:String="Jane wanjiku"
+        println(name)
+
+    var age:Int=23
+        println(age)
+
+    var phoneNumber=("0792903185")
+        println(phoneNumber)
+
+    var weight:Int=47
+        println(weight)
+
+    val Kenyan:Boolean=true
+        println(Kenyan)
+
+    }
